a1bc5c599d7eea6cd19dee018d3474238e590932
Sadek <sade0027@stud.kea.dk>
2023-11-07 18:14:35 +0100

    Package structure created

---
+++ src/test/java/com/example/stoicopenai_backend/StoicOpenAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stoicopenai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoicOpenAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
