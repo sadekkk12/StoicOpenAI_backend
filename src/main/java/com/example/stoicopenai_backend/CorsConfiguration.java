f263ce801778ae2950306b6c5ef0ee33a39d7d37
sadekkk12 <113339821+sadekkk12@users.noreply.github.com>
2024-03-15 16:00:21 +0100

    Update CorsConfiguration.java

--- src/main/java/com/example/stoicopenai_backend/CorsConfiguration.java
+++ src/main/java/com/example/stoicopenai_backend/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://stoicopenai.sadekdev.com")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
